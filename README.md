8ae5537c67b125585b5bef7dd7b6f297fa4495ca
Your Name <irving.jem@gmail.com>
2022-08-05 08:40:51 -0700

    added file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # udemy-python-stuff
+
+Just a place to hold all my code from 100 days of python from Udemy
---
+++ python-learning.py
@@ -0,0 +1,8 @@
+# print(round(8 / 3))
+
+# for loops 
+# simple loop
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits: 
+    print(fruit)
+    print(fruit + " Pie")
