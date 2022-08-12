b04f7a8dc2fb7a2833b84e8d9d5551793ff5f695
Your Name <irving.jem@gmail.com>
2022-08-12 09:03:43 -0700

    working with OOP in python and turtle.py

---
+++ OOP/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+gary = Turtle()
+print(gary)
+gary.shape("turtle")
+gary.color("green")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
